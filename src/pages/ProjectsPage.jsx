b4ad5316085248fc6f4af963699388220a7baa94
ANKIT KUMAR VISHWAKARMA <68489896+ankitkvishwakarma@users.noreply.github.com>
2025-07-26 01:27:39 +0530

    Update ProjectsPage.jsx

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,12 +2,14 @@ import Header from '../components/Header';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 
-const ProjectsPage = () => (
-  <>
-    <Header />
-    <Projects />
-    <Footer />
-  </>
-);
+const ProjectsPage = () => {
+  return (
+    <>
+      <Header />
+      <Projects />
+      <Footer />
+    </>
+  );
+};
 
 export default ProjectsPage;
